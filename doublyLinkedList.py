b71dd8f2cb90e476ce6f82d6b6c99792fc8f65a6
viktorvoltz <vctrchinyeaka@gmail.com>
2021-10-03 01:39:03 +0100

    positional

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -73,7 +73,7 @@ class LinkedDeque(_DoublyLinkedBase):
 
 class PositionalList(_DoublyLinkedBase):
 
-    class position:
+    class Position:
         def __init__(self, container, node):
             self._container = container
             self._node = node
@@ -86,6 +86,43 @@ class PositionalList(_DoublyLinkedBase):
 
         def __ne__(self, other):
             return not(self == other)
+
+    def _validate(self, p):
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be a proper Position type')
+
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+
+        if p._node._next is None:
+            raise ValueError('p is no longer valid')
+        return p._node
+
+    def _make_position(self, node):
+        if node is self._header or node is self._trailer:
+            return None
+        else:
+            return self.Position(self, node)
+
+    def first(self):
+        return self._make_position(self._header._next)
+
+    def last(self):
+        return self._make_position(self._trailer._prev)
+
+    def before(self, p):
+        node = self._validate(p)
+        return self._make_position(node._prev)
+
+    def after(self, p):
+        node = self._validate(p)
+        return self._make_position(node._next)
+
+    def __iter__(self):
+        cursor = self.first()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.after(cursor)
 
 
 
