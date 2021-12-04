de16ca3ef961bd3bb4bbf681470cd8b5f2372456
viktorvoltz <vctrchinyeaka@gmail.com>
2021-12-04 23:20:57 +0100

    search algorithm in c

---
+++ search.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(){
+    int c, first, last, middle, n, search, array[100];
+
+    printf("enter number of element: \n");
+    scanf("%d", &n);
+    printf("enter %d integer: \n", n);
+    
+    for(c = 0; c < n; c++){
+    	scanf("%d", &array[c]);
+	}
+	
+	printf("Enter the value to find: \n");
+	scanf("%d", &search);
+	
+	first = 0;
+	last = n - 1;
+	middle = (first + last) / 2;
+	while (first <= last){
+		if (array[middle] < search){
+			first = middle + 1;
+			middle = (first + last)/2;
+			printf("...searching \n");	
+		}
+		else if(array[middle] == search){
+			printf("%d is present at index %d \n", search, middle + 1);
+			break;
+		}
+		else{
+			last = middle - 1;
+			middle = (first + last)/2;
+			printf("...searching \n");
+		}
+		
+	}
+	
+	if (first > last){
+		printf("%d not found, search terminated", search);
+	}
+	
+    
+    return 0;
+}
